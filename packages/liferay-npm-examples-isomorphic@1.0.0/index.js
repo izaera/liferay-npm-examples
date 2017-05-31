d75527c796448aeb01eb4c1b48466f946f437ab2
Iván Zaera Avellón <izaera@gmail.com>
2017-05-31 17:22:27 +0200

    Add an example isomorphic NPM package

---
+++ packages/liferay-npm-examples-isomorphic@1.0.0/index.js
@@ -0,0 +1,39 @@
+var os = require('os');
+var path = require('path');
+var querystring = require('querystring');
+var util = require('util');
+
+module.exports = function(out) {
+	out.log('Scheduling code to run on `nextTick`');
+
+	process.nextTick(() => {
+		out.log('');
+		out.log(
+			util.format(
+				'Formatted string [%s] and number [%d]',
+				'meaning of life',
+				42
+			)
+		);
+
+		out.log('');
+		out.log('Number of CPUs: ' + os.cpus().length);
+		out.log('Total memory: ' + os.totalmem());
+		out.log('OS Type: ' + os.type());
+		out.log('Uptime: ' + os.uptime());
+
+		out.log('');
+		out.log(
+			querystring.stringify({
+				an: 'object',
+				stringified: 'with querystring module',
+			})
+		);
+
+		var file = '/usr/local/bin/bash';
+		out.log('');
+		out.log('File path:    ' + file);
+		out.log('  · dirname:  ' + path.dirname(file));
+		out.log('  · basename: ' + path.basename(file));
+	});
+};
